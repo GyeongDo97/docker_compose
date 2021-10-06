6f998b8f9331725fa72cfad3099b9b6d59dfa542
GyeongDo97 <gds97@naver.com>
2021-10-06 19:49:27 +0900

    docker_compose_21.10.06

---
+++ docker/mysql-init-files/init.sql
@@ -0,0 +1,11 @@
+create database db01;
+use db01;
+create table user(
+    id varchar(500),
+	pw varchar(500),
+	name varchar(500),
+	addr varchar(500),
+    tel varchar(500),
+    gen varchar(500),
+	PRIMARY KEY (id)
+);
